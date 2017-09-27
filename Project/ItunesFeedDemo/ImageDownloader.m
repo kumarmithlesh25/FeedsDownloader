c968a0c95f395cf1313c5aff4600379495f380cc
Mithlesh Kumar Jha <mithleshj9@gmail.com>
2017-09-28 01:57:34 +0530

    Commented

--- Project/ItunesFeedDemo/ImageDownloader.m
+++ Project/ItunesFeedDemo/ImageDownloader.m
@@ -29,7 +29,7 @@
         if (error == nil) {
             [[NSOperationQueue mainQueue] addOperationWithBlock: ^{
                 
-                
+                // This change is done on local branch. We'll now create a pull request and merge it back to master.
                 
                 // Set appIcon and clear temporary data/image
                 UIImage *image = [[UIImage alloc] initWithData:data];
